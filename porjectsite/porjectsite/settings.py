f6210f00a103bf8345f2832fac4968e357263b1b
radz <20228004@psu.palawan.edu.ph>
2025-04-12 23:21:56 +0800

    i made this on the PSUspehere tas kinopy ko lang here para mahiwalay

--- porjectsite/porjectsite/settings.py
+++ porjectsite/porjectsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'FIREAPP',  # Custom app for the project
+    'widget_tweaks',  # Custom app for the project
 ]
 
 MIDDLEWARE = [
