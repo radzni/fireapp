a17e403ef8db05a711b412d32789db0615435454
radz <20228004@psu.palawan.edu.ph>
2025-04-15 13:27:24 +0800

    fire station map and chart

---
+++ projectsite/projectsite/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+
+from fire.views import HomePageView
+from fire import views
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarbySeverity
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
+
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
+    path('line_chart/', LineCountbyMonth, name='chart'),
+    path('multiline_chart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiple_bar_chart/', multipleBarbySeverity, name='chart'),
+    path('stations', views.map_station, name='map-station'),
+
+]
